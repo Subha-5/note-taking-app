22754f34851efef41af58c86c686a317180f174f
Subha Sadhu <subhasadhu5@gmail.com>
2023-10-09 01:37:17 +0530

    Notes and Tag creation logic

--- src/pages/NewNote.tsx
+++ src/pages/NewNote.tsx
@@ -1,8 +1,15 @@
+import { NoteData, Tag } from "../App";
 import NoteForm from "../components/NoteForm";
 
-export default function NewNote() {
+type NewNoteProps = {
+    onSubmit: (data: NoteData) => void,
+    onAddTag: (tag: Tag) => void,
+    availableTags: Tag[]
+}
+
+export default function NewNote({onSubmit, onAddTag, availableTags}: NewNoteProps) {
     return <>
         <h1 className="mb-4">NewNote</h1>
-        <NoteForm />
+        <NoteForm onSubmit={onSubmit} onAddTag={onAddTag} availableTags={availableTags} />
     </>;
 }
