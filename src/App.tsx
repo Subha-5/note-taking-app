9bda2853b76b1b6d3bb515605bdbb0286514ee9d
Subha Sadhu <subhasadhu5@gmail.com>
2023-10-08 14:09:59 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container } from "react-bootstrap";
+
+function App() {
+  return (
+    <Container>
+      
+    </Container>
+  )
+}
+
+export default App
